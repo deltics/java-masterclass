623cb3b20453be593ff550b89684f6d55592c8f5
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-10 17:00:00 +1300

    Sec.4.Ex1 - SpeedConverter solution

---
+++ SpeedConverter/src/nz/co/deltics/udemy/javamasterclass/Main.java
@@ -0,0 +1,8 @@
+package nz.co.deltics.udemy.javamasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SpeedConverter.toMilesPerHour(25.42));
+    }
+}
---
+++ SpeedConverter/src/nz/co/deltics/udemy/javamasterclass/SpeedConverter.java
@@ -0,0 +1,22 @@
+package nz.co.deltics.udemy.javamasterclass;
+
+public class SpeedConverter {
+
+    private static final double MilesToKms = 1.609d;
+
+    public static void printConversion(double kph) {
+        long mph = toMilesPerHour(kph);
+
+        String s = (mph != -1) ? kph + " km/h = " + mph + " mi/h" : "Invalid Value";
+        System.out.println(s);
+    }
+
+
+    public static long toMilesPerHour(double kph) {
+        if (kph < 0)
+            return -1;
+
+        return Math.round(kph / MilesToKms);
+    }
+}
+
