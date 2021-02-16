137585be56f27ddc50db4c6ddb22fbcce2836fd1
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-16 13:49:28 +1300

    feat: Section 7 exercise solutions

---
+++ section-7-exercises/src/nz/co/deltics/udemy/javamasterclass/Hamburger.java
@@ -0,0 +1,69 @@
+package nz.co.deltics.udemy.javamasterclass;
+
+public class Hamburger {
+
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+
+        this.addition1Price = -1;
+        this.addition2Price = -1;
+        this.addition3Price = -1;
+        this.addition4Price = -1;
+    }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.addition1Name = name;
+        this.addition1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
+
+    public double itemizeHamburger() {
+
+        System.out.println(this.name + " burger on a " + this.breadRollType + " roll with " + this.meat + ", price is " + this.price);
+
+        if (addition1Price != -1)
+            System.out.println("Added " + addition1Name + " for an extra " + addition1Price);
+        if (addition2Price != -1)
+            System.out.println("Added " + addition2Name + " for an extra " + addition2Price);
+        if (addition3Price != -1)
+            System.out.println("Added " + addition3Name + " for an extra " + addition3Price);
+        if (addition4Price != -1)
+            System.out.println("Added " + addition4Name + " for an extra " + addition4Price);
+
+        return this.price
+                + (this.addition1Price != -1 ? this.addition1Price : 0)
+                + (this.addition2Price != -1 ? this.addition2Price : 0)
+                + (this.addition3Price != -1 ? this.addition3Price : 0)
+                + (this.addition4Price != -1 ? this.addition4Price : 0);
+    }
+}
---
+++ section-7-exercises/src/nz/co/deltics/udemy/javamasterclass/HealthyBurger.java
@@ -0,0 +1,40 @@
+package nz.co.deltics.udemy.javamasterclass;
+
+public class HealthyBurger extends Hamburger {
+
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy burger", meat, price, "Brown rye");
+
+        this.healthyExtra1Price = -1;
+        this.healthyExtra2Price = -1;
+    }
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        double price = super.itemizeHamburger();
+
+        if (healthyExtra1Price != -1)
+            System.out.println("Added " + healthyExtra1Name + " for an extra " + healthyExtra1Price);
+        if (healthyExtra2Price != -1)
+            System.out.println("Added " + healthyExtra2Name + " for an extra " + healthyExtra2Price);
+
+        return price
+                + (this.healthyExtra1Price != -1 ? this.healthyExtra1Price : 0)
+                + (this.healthyExtra2Price != -1 ? this.healthyExtra2Price : 0);
+    }
+}
